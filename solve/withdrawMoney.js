48d7c342aad749411407581aad29b4a1156c6d03
Francisca <fransanalves@outlook.com>
2022-05-23 17:22:17 -0300

    problem solved withdraw money

--- solve/withdrawMoney.js
+++ solve/withdrawMoney.js
@@ -1,7 +1,15 @@
-function withdrawMoney() {
-    let availableMoney = [2, 5, 10, 20, 50, 100];
-    
-    return availableMoney;
+function withdrawMoney(money) {
+    let availableMoney = [1, 2, 5, 10, 20, 50, 100];
+    let arr = [];
 
+    while (money > 0) {
+        let arrNum = availableMoney.filter((item) => item <= money);
+        let numMax = Math.max(...arrNum);
+        arr.push(numMax);
+        money = money - numMax;
+    }
+
+    return arr;
 }
+
 module.exports = withdrawMoney;
--- test/withdrawMoney.test.js
+++ test/withdrawMoney.test.js
@@ -2,6 +2,11 @@ const withdrawMoney = require('../solve/withdrawMoney')
 
 describe('', () => {
     test('should return an array with a minimum of cedulas', () => {
-        expect(withdrawMoney()).toBe([100, 100]);
+        expect(withdrawMoney(3)).toStrictEqual([2, 1]);
+        expect(withdrawMoney(8)).toStrictEqual([5, 2, 1]);
+        expect(withdrawMoney(13)).toStrictEqual([10, 2, 1]);
+        expect(withdrawMoney(55)).toStrictEqual([50, 5]);
+        expect(withdrawMoney(175)).toStrictEqual([100, 50, 20, 5]);
+        expect(withdrawMoney(365)).toStrictEqual([100, 100, 100, 50, 10, 5]);
     });
 });
