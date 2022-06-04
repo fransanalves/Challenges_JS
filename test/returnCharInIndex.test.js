06646f0bb592152210c0191bff5f770bcaad4f26
Francisca <fransanalves@outlook.com>
2022-06-04 19:38:08 -0300

    indentation adjustment

---
+++ test/returnCharInIndex.test.js
@@ -0,0 +1,12 @@
+const returnCharInIndex = require('../solve/returnCharInIndex');
+
+describe('', () => {
+    test('should return the character in the given index.', () => {
+        expect(returnCharInIndex("olá, mundo!", 1)).toBe("o");
+        expect(returnCharInIndex("olá, mundo!", 2)).toBe("l");
+        expect(returnCharInIndex("olá, mundo!", 3)).toBe("á");
+        expect(returnCharInIndex("olá, mundo!", 4)).toBe(",");
+        expect(returnCharInIndex("olá, mundo!", 5)).toBe("m");
+        expect(returnCharInIndex("olá, mundo!", 6)).toBe("u");
+    });
+});
